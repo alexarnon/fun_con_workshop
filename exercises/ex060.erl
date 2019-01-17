1267f832f15af83152bd7c2daf8c232a89e1c5e4
alexarnon <alex.arnon@nielsen.com>
2019-01-17 15:33:40 +0200

    ex060

---
+++ exercises/ex060.erl
@@ -0,0 +1,18 @@
+-module(ex060).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export([unspace/1]).
+
+
+unspace(Str) ->
+    error(not_implemented).
+
+
+%% Unit Tests
+
+repeat_test_() ->
+    [?_assertEqual("", unspace("")),
+     ?_assertEqual("", unspace("   ")),
+     ?_assertEqual("helpme", unspace("  hel p   me "))
+    ].
