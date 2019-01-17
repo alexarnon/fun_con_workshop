192c35a342268856bfbcd3144c78fce6cd77a9c9
alexarnon <alex.arnon@nielsen.com>
2019-01-17 15:12:42 +0200

    ex030,ex040

---
+++ exercises/ex030.erl
@@ -0,0 +1,24 @@
+-module(ex030).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export([sum/1]).
+
+
+sum(N) -> 
+    sum(N, 0).
+     
+sum(N, A) ->         
+    error(not_implemented).
+    
+
+%% Unit Tests
+
+sum_test_() ->
+    [?_assertEqual(0, sum(0)),
+     ?_assertEqual(1, sum(1)),
+     ?_assertEqual(3, sum(2)),
+     ?_assertEqual(6, sum(3)),
+     ?_assertEqual(10, sum(4))
+    ].
+    
---
+++ exercises/ex040.erl
@@ -0,0 +1,18 @@
+-module(ex040).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export([len/1]).
+
+
+len(L) ->
+    error(not_implemented).
+
+
+%% Unit Tests
+
+repeat_test_() ->
+    [?_assertEqual(0, len([])),
+     ?_assertEqual(1, len([aaa])),
+     ?_assertEqual(4, len([aaa, bbb, ccc, "hello"]))
+    ].
